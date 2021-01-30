2a1f31f0b246a9e8456315b702747c83c812f907
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-01-30 17:11:25 +0800

    added container for photos component

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -4,14 +4,16 @@ function Photos() {
   return (
     <>
       <Header />
-      <img
-        src='https://images.pexels.com/photos/1527603/pexels-photo-1527603.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-        alt='yummy noodles'
-      />
-      <img
-        src='https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-        alt='yummy bread'
-      />
+      <div className='container'>
+        <img
+          src='https://images.pexels.com/photos/1527603/pexels-photo-1527603.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
+          alt='yummy noodles'
+        />
+        <img
+          src='https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
+          alt='yummy bread'
+        />
+      </div>
     </>
   )
 }
