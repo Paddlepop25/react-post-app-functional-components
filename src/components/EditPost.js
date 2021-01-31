49d9e9571e188cf97afc803b378ce491e33b14a2
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-01-31 13:26:27 +0800

    stimulated editpost

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -5,13 +5,18 @@ import posts from '../postsData.json'
 function EditPost(props) {
   const [carb, setCarb] = useState('')
   const [yummies, setYummies] = useState('')
-  const { id } = props.match.params
 
   useEffect(() => {
-    let post = posts.filter((post) => post.id === Number(id))[0]
-    setCarb(post.carb)
-    setYummies(post.yummies)
-  }, [])
+    const { id } = props.match.params
+
+    if (id > 3) {
+      alert('Please choose RICE, NOODLES or BREAD\nfor the edit page')
+    } else {
+      let post = posts.filter((post) => post.id === Number(id))[0]
+      setCarb(post.carb)
+      setYummies(post.yummies)
+    }
+  }, [setCarb, setYummies])
 
   const editPost = (event) => {
     if (event.target.name === 'carb') {
@@ -22,45 +27,53 @@ function EditPost(props) {
     }
   }
 
+  const saveEditPost = (event) => {
+    event.preventDefault()
+    console.log('saved edited post') // save to database in real life
+    setCarb('')
+    setYummies('')
+    props.history.push('/')
+  }
+
   return (
     <>
       <Header />
       <div className='container'>
-        {/* <form onSubmit={saveEditPost}> */}
-        <h3 style={{ margin: '15px 0' }}>Edit post</h3>
-        <code style={code}>
-          Please choose RICE, NOODLES or BREAD from the homepage
-        </code>
+        <form onSubmit={saveEditPost}>
+          <h3 style={{ margin: '15px 0' }}>Edit post</h3>
+          <code style={code}>
+            Please choose RICE, NOODLES or BREAD from the homepage
+          </code>
 
-        <br />
-        <br />
-        <label>Carb: </label>
-        <input
-          name='carb'
-          value={carb}
-          onChange={editPost}
-          maxLength='15'
-          style={inputStyle}
-        />
-        <br />
-        <br />
+          <br />
+          <br />
+          <label>Carb: </label>
+          <input
+            name='carb'
+            value={carb}
+            onChange={editPost}
+            maxLength='15'
+            style={inputStyle}
+          />
+          <br />
+          <br />
 
-        <label>Yummies: </label>
-        <input
-          name='yummies'
-          value={yummies}
-          onChange={editPost}
-          maxLength='100'
-          style={inputStyle}
-        />
+          <label>Yummies: </label>
+          <input
+            name='yummies'
+            value={yummies}
+            onChange={editPost}
+            maxLength='100'
+            style={inputStyle}
+          />
 
-        <br />
-        <br />
+          <br />
+          <br />
 
-        <button style={saveBtn}>
-          {/* {state.post.carb !== '' ? 'Save post' : 'Back to homepage'} */}
-        </button>
-        {/* </form> */}
+          <button style={saveBtn}>
+            {carb !== '' || yummies !== '' ? 'Save post' : 'Back to homepage'}
+          </button>
+        </form>
       </div>
     </>
   )
@@ -77,7 +90,7 @@ const inputStyle = {
 
 const saveBtn = {
   background: '#fcba03',
-  color: '#fff',
+  color: '#333',
   border: 'none',
   padding: '5px 9px',
   borderRadius: '20px',
