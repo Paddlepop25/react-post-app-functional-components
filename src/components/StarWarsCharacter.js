7db6028b285874b4c0cd76b75d628bb96fa67d3e
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-01-31 15:06:36 +0800

    included loading spinner

--- src/components/StarWarsCharacter.js
+++ src/components/StarWarsCharacter.js
@@ -1,37 +1,62 @@
 import React, { useState, useEffect } from 'react'
 import Header from './Header'
-import { BarLoader } from 'react-spinners'
+import { css } from '@emotion/core'
+import BarLoader from 'react-spinners/BarLoader'
 
 function StarWarsCharacter() {
-  const [loading, setLoading] = useState(true)
+  const [isLoading, setisLoading] = useState(true)
   const [starWarsCharacter, setstarWarsCharacter] = useState({})
 
   useEffect(() => {
     const randomId = Math.floor(Math.random() * 80) + 1
     fetch(`https://swapi.dev/api/people/${randomId}`)
       .then((result) => result.json())
-      .then((data) => setstarWarsCharacter(data))
-  }, [loading])
+      .then((data) => {
+        setisLoading(false)
+        setstarWarsCharacter(data)
+      })
+  }, [isLoading])
   return (
     <>
       <Header />
       <div style={cardStyle}>
+        <BarLoader color='#f00af0' loading={isLoading} css={override} />
+
+        {isLoading ? 'Loading...' : null}
         <h3>
           Name: <code style={codeStyle}>{starWarsCharacter.name}</code>
+          <br />
+          Height: <code style={codeStyle}>{starWarsCharacter.height}</code>
+          <br />
+          Mass: <code style={codeStyle}>{starWarsCharacter.mass}</code>
+          <br />
+          Skin color:{' '}
+          <code style={codeStyle}>{starWarsCharacter['skin_color']}</code>
+          <br />
+          Birth year:{' '}
+          <code style={codeStyle}>{starWarsCharacter['birth_year']}</code>
         </h3>
       </div>
     </>
   )
 }
 
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`
+
 const cardStyle = {
-  border: '1px dashed red',
+  // border: '1px dashed red',
   width: '50%',
-  margin: '0 auto',
+  margin: '3rem auto',
+  textAlign: 'center',
 }
 
 const codeStyle = {
   fontSize: '2rem',
   color: '#44c1c9',
+  paddingLeft: '5px',
 }
 export default StarWarsCharacter
